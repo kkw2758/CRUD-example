db8aab7f8cf565cb51275e27dd5d8279cfb97221
kkw2758 <dtc3229@gmail.com>
2024-03-12 14:07:03 +0900

    feat : Question 엔티티를 Subject를 이용해서 조회하는 메서드 추가

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -1,2 +1,7 @@
-package com.mysite.sbb;public class QuestionRepository {
+package com.mysite.sbb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Question findBySubject(String subject);
 }
