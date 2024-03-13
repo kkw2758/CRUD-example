28527713ab1b371f83b69410ed8c6a0883a40f01
kkw2758 <dtc3229@gmail.com>
2024-03-13 10:12:48 +0900

    feat : 질문 목록 확인 메서드 추가

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,2 +1,15 @@
-package com.mysite.sbb.question;public class QuestionController {
+package com.mysite.sbb.question;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class QuestionController {
+
+    @GetMapping("/question/list")
+    @ResponseBody
+    public String list() {
+        return "question_list";
+    }
 }
