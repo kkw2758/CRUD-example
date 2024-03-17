1ac8e898e9659e18c901e8aa66148710c6dd1f3c
kkw2758 <dtc3229@gmail.com>
2024-03-17 15:10:16 +0900

    feat : 질문, 답변 등록 마크다운 문법 적용

---
+++ src/main/java/com/mysite/sbb/CommonUtil.java
@@ -0,0 +1,17 @@
+package com.mysite.sbb;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommonUtil {
+    //마크다운 텍스트를 HTML 문서로 변환하여 리턴한다.
+    public String markdown(String markdown) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdown);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
+    }
+}
